b2667f98b5ca0e737c6b68f10aba5fb1882b2ebb
shivyansh21 <78162000+shivyansh21@users.noreply.github.com>
2022-11-08 14:29:57 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,64 @@
+// Declaring api information
+const api = {
+    apiKey: '2f7dc9aa4ed4eaec932e99724f9cd6a1',
+    url: 'api.openweathermap.org/data/2.5/'
+}
+
+// Fetching button, input and diffrent h1 tags
+
+const btn = document.getElementById('btn');
+const input = document.getElementById('input');
+const ul = document.querySelector('ul');
+const h1 = document.querySelector('h1');
+const h2 = document.querySelector('h2');
+const h3 = document.querySelector('h3');
+const h4 = document.querySelector('h4');
+const img = document.querySelector('img');
+const p = document.querySelector('p');
+
+// Adding event listners to button
+
+btn.addEventListener('click', weather);
+
+// Adding event listners for enter key
+
+input.addEventListener('keypress',function (e) {
+    if (e.keyCode === 13) {
+        weather();
+    }
+});
+
+// Declaring weather function
+
+function weather() {
+    let cityName = input.value;
+    fetch(`https://${api.url}weather?q=${cityName}&units=metric&APPID=${api.apiKey}`)
+        .then((apiData) => {
+            console.log(apiData);
+            return apiData.json();
+        })
+        .then((realData) => {
+
+            h3.innerText = `${realData.name}, ${realData.sys.country}`;
+            h1.innerHTML = `${Math.round(realData.main.temp)}<span>&#176;C </span>`;
+            p.innerText = currentDate();
+            h2.innerText = realData.weather[0].main;
+            h4.innerHTML = `${Math.round(realData.main.temp_min)}<span>&#176;C </span> /  ${Math.round(realData.main.temp_max)}<span>&#176;C </span>`;
+            var iconcode = realData.weather[0].icon;
+            var iconurl = "http://openweathermap.org/img/w/" + iconcode + ".png";
+            img.src = iconurl;
+            input.value = "";
+        }).catch((err) => {
+            console.log(err);
+        });
+}
+
+// Declaring weather function
+
+function currentDate() {
+    var WeekDay = new Date;
+    let week = ["Sunday", "Monday", "Tuesday", "Wednday", "Thrusday", "Friday", "Saturday"];
+    let month = ["January", "Febuary", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    let day = WeekDay.getDate();
+    return `${week[WeekDay.getDay()]}/ ${day}  ${month[WeekDay.getMonth()]} / ${WeekDay.getFullYear()}`;
+}
